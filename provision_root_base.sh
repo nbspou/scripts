e352f12ff0f963d83b448088fb7034cc06f47751
Jan Boon <kaetemi@gmail.com>
2017-09-04 20:27:14 +0800

    Update provision_root_base.sh

--- provision_root_base.sh
+++ provision_root_base.sh
@@ -4,6 +4,8 @@ set -x
 apt-get update
 apt-get dist-upgrade -y
 
+# See: https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-16-04
+
 fallocate -l 8G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
@@ -25,10 +27,13 @@ apt-get install prometheus-node-exporter -y
 
 curl -sSL https://raw.githubusercontent.com/nbspou/scripts/master/provision_me_base.sh | bash
 
+# See: https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-16-04
+
 adduser --disabled-password --gecos "" me
 mkdir /home/me/.ssh
 chown me:me /home/me/.ssh
 cp /root/.ssh/authorized_keys /home/me/.ssh/authorized_keys
 chown me:me /home/me/.ssh/authorized_keys
+usermod -aG sudo me
 
 curl -sSL https://raw.githubusercontent.com/nbspou/scripts/master/provision_me_base.sh | su me -c bash
