b64a558e39c6a995a2ed8cb484d568ebba2310c0
Jan Boon <kaetemi@gmail.com>
2017-09-04 19:22:21 +0800

    Update provision_root_dogcat.sh

--- provision_root_dogcat.sh
+++ provision_root_dogcat.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -x
 
+cd ~
 git clone -q https://github.com/nbspou/dogcat.git dogcat
 cd dogcat
 mkdir build
 cd build
 cmake ..
+make
 cd ..
 cd ..
 cp ~/dogcat/build/bin/dogcat /usr/bin/dogcat
