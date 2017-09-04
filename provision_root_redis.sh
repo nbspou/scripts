298fd799914d7fbca173f6da057ca292fefd7be3
Jan Boon <kaetemi@gmail.com>
2017-09-04 17:37:10 +0800

    Update provision_root_redis.sh

--- provision_root_redis.sh
+++ provision_root_redis.sh
@@ -10,8 +10,16 @@ apt-get dist-upgrade -y
 sysctl vm.overcommit_memory=1
 echo 'vm.overcommit_memory=1' | sudo tee -a /etc/sysctl.conf
 
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+# echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+# See: https://docs.mongodb.com/manual/tutorial/transparent-huge-pages/
+
+wget https://raw.githubusercontent.com/nbspou/scripts/master/data/disable-transparent-hugepage
+mv disable-transparent-hugepage /etc/init.d/disable-transparent-hugepages
+chmod 755 /etc/init.d/disable-transparent-hugepages
+bash /etc/init.d/disable-transparent-hugepages
 
 # See: https://launchpad.net/~chris-lea/+archive/ubuntu/redis-server
 
 apt-get install redis-server -y
+systemctl enable redis-server
