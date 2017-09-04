cdcf55c29686880c5112dd197fd733a5bb9aae50
Jan Boon <kaetemi@gmail.com>
2017-09-04 18:42:14 +0800

    Update provision_root_redis.sh

--- provision_root_redis.sh
+++ provision_root_redis.sh
@@ -31,3 +31,7 @@ GOPATH=/root/redis_exporter go get
 GOPATH=/root/redis_exporter go build
 cd ..
 cp /root/redis_exporter/redis_exporter /usr/bin/prometheus-redis-exporter
+wget https://raw.githubusercontent.com/nbspou/scripts/master/data/redis_exporter.service
+mv redis_exporter.service /etc/systemd/system/redis_exporter.service
+systemctl enable redis_exporter
+systemctl start redis_exporter
