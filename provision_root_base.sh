ee8ec3e2aac33e4afb5ab47216a0946fd4d87c7f
Jan Boon <kaetemi@gmail.com>
2017-09-04 17:09:10 +0800

    Create provision_root_base.sh

---
+++ provision_root_base.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -x
+
+apt-get update
+apt-get dist-upgrade -y
+
+fallocate -l 8G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+swapon --show
+free -h
+
+sysctl vm.swappiness=10
+echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+
+sysctl vm.vfs_cache_pressure=50
+echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
+
+curl -sL https://deb.nodesource.com/setup_6.x | bash
+apt-get install nodejs build-essential git mercurial cmake -y
+
+adduser --disabled-password --gecos "" me
+mkdir /home/me/.ssh
+chown me:me /home/me/.ssh
+cp /root/.ssh/authorized_keys /home/me/.ssh/authorized_keys
+chown me:me /home/me/.ssh/authorized_keys
