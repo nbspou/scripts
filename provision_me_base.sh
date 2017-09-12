30a17f350deb588b058c0123fd4b0335b9fb739e
Jan Boon <kaetemi@gmail.com>
2017-09-12 19:18:44 +0800

    Update provision_me_base.sh

--- provision_me_base.sh
+++ provision_me_base.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 
-sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc
-source ~/.bashrc
+# sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc
+# source ~/.bashrc
 
 yes "y" | ssh-keygen -t rsa -N "" -C $HOSTNAME"-"$USER -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
