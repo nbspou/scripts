aa17b77d69c69bafa730a4c221706ebd49c00046
Jan Boon <kaetemi@gmail.com>
2018-09-22 18:30:05 +0800

    Update provision_me_pm2.sh

--- provision_me_pm2.sh
+++ provision_me_pm2.sh
@@ -9,3 +9,6 @@ set -x
 
 sudo npm install -g pm2
 pm2 startup
+
+# [PM2] To setup the Startup Script, copy/paste the following command:
+# sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u me --hp /home/me
