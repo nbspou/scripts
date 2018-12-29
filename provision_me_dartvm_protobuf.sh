d7cd762d73f68c8dbbc6a469b4dfc9aded7877e5
Jan Boon <kaetemi@gmail.com>
2018-12-29 17:07:41 +0800

    Update provision_me_dartvm_protobuf.sh

--- provision_me_dartvm_protobuf.sh
+++ provision_me_dartvm_protobuf.sh
@@ -9,7 +9,7 @@ sudo apt-get install protobuf-compiler apt-transport-https
 sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
 sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
 
-sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list > /etc/apt/sources.list.d/dart_unstable.list'
+# sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list > /etc/apt/sources.list.d/dart_unstable.list'
 
 sudo apt-get update
 sudo apt-get install dart
