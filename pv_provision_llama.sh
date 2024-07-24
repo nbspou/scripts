9e642be5315f684dce27a760ec7e0f3e8339cd78
Jan Boon <jan.boon@kaetemi.be>
2024-07-25 05:45:56 +0800

    Update pv_provision_llama.sh

--- pv_provision_llama.sh
+++ pv_provision_llama.sh
@@ -12,7 +12,7 @@ if [ ! -f "$SETUP_FLAG" ]; then
     apt-get update
 
     # Install necessary packages
-    apt-get install aptitude libcurl4-openssl-dev libomp-dev clang cmake ninja-build nano nvtop htop ncurses-term lshw -y
+    apt-get install aptitude libcurl4-openssl-dev libomp-dev clang cmake ninja-build nano nvtop htop ncurses-term lshw screen aria2c -y
 
     # Create flag file to indicate setup is done
     touch "$SETUP_FLAG"
