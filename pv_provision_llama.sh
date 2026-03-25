143449fb6cb9b83e20d3035221fd21910f186f8d
Jan Boon <jan.boon@kaetemi.be>
2026-03-25 15:52:02 +0800

    Add additional packages to installation script

--- pv_provision_llama.sh
+++ pv_provision_llama.sh
@@ -30,6 +30,7 @@ if [ ! -f "$SETUP_FLAG" ]; then
 
     # Install necessary packages
     apt-get install aptitude libcurl4-openssl-dev libomp-dev clang cmake ninja-build nano nvtop htop ncurses-term lshw screen aria2 unzip libstdc++-12-dev caddy -y
+    apt-get install google-perftools git python3.10 python3.10-distutils python3.10-venv ffmpeg -y
 
     # Create flag file to indicate setup is done
     touch "$SETUP_FLAG"
